c6669b44c2e07db4775ecbb71c64818e72796c85
pitzee <pitzee421@gmail.com>
2025-02-06 11:47:38 +0000

    navbar home icon transtion is added

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import Herosection from "./components/Herosection";
 import Movies from "./components/Movies";
 import Navbar from "./components/Navbar";
 import Search from "./components/Search";
-import SearchedMovies from "./components/SearchedMovies";
 import TrendingMovies from "./components/TrendingMovies";
 
 const App = () => {
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,15 @@
 import { Link } from "react-router-dom"; 
+import { GoHome } from "react-icons/go";
 
 const Navbar = () => {
   return (
     <div className="flex items-center h-15 bg-blue-950 justify-between ">
-      <Link to="/">
+      
+      <Link to="/" className="flex flex-row items-center hover:translate-y-1">
+       <GoHome className="text-white text-2xl"/>
+       
       <p className="pl-2 text-amber-50 font-extrabold">MOVIE HUB</p>
       </Link>
-      
       <div className="flex justify-between space-x-1 text-white font-bold pr-2">
         <Link to="/" >
         <p>Movies</p>
