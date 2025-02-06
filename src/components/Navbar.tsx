c5ed37678f25eec581ac2ff77892bc5e5524f127
pitzee <pitzee421@gmail.com>
2025-02-06 11:25:19 +0000

    navigation to home page is added

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,17 @@
+import { Link } from "react-router-dom"; 
+
 const Navbar = () => {
   return (
     <div className="flex items-center h-15 bg-blue-950 justify-between ">
+      <Link to="/">
       <p className="pl-2 text-amber-50 font-extrabold">MOVIE HUB</p>
+      </Link>
+      
       <div className="flex justify-between space-x-1 text-white font-bold pr-2">
+        <Link to="/" >
         <p>Movies</p>
+        </Link>
+        
         <p>Tv shows</p>
       </div>
     </div>
--- src/components/SearchedMovies.tsx
+++ src/components/SearchedMovies.tsx
@@ -1,8 +1,10 @@
+import Navbar from "./Navbar";
 import SearchedMoviesGrid from "./SearchedMoviesGrid";
 
 const SearchedMovies = () => {
   return (
     <div>
+      <Navbar />
       <p className="text-2xl font-bold mb-5">Searched movies list</p>
       <SearchedMoviesGrid />
     </div>
