b8f38904d7e419655ce8d16c30ca0290811d4e65
pitzee <pitzee421@gmail.com>
2025-02-07 12:10:59 +0000

    overview is added for movies

--- src/components/MovieGrid.tsx
+++ src/components/MovieGrid.tsx
@@ -1,9 +1,19 @@
 import moment from "moment";
 import useMovies from "../hooks/useMovies";
 import MoviesSkeleton from "./MoviesSkeleton";
+import { useState } from "react";
+import { IoMdClose } from "react-icons/io";
 
 const MovieGrid = () => {
   const { movies, error, isLoading } = useMovies();
+
+  const [id, setId] = useState(0);
+  const [closeOverview, setCloseOverview] = useState(true);
+
+  const HandleClick = (movieId: number) => {
+    setId(movieId);
+    setCloseOverview(true);
+  };
 
   return (
     <>
@@ -11,7 +21,12 @@ const MovieGrid = () => {
       {error && <p>{error}</p>}
       <div className="flex-grow grid grid-cols-1 lg:grid-cols-5 md:grid-cols-3 gap-4">
         {movies.map((movie) => (
-          <div key={movie.id}>
+          <div
+            key={movie.id}
+            onClick={() => {
+              HandleClick(movie.id);
+            }}
+          >
             <div className="flex flex-row h-60 w-44 rounded-xl">
               <img
                 src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
@@ -27,6 +42,23 @@ const MovieGrid = () => {
                   : "Date not available"}
               </p>
             </div>
+            {closeOverview
+              ? id == movie.id && (
+                  <div className="fixed inset-0 flex flex-col lg:flex-row lg:items-center lg:justify-center bg-black bg-opacity-50 z-50 p-4 gap-4">
+                    <IoMdClose
+                      className="text-white text-2xl absolute top-4 right-4 z-60 cursor-pointer"
+                      onClick={() => setCloseOverview(false)}
+                    />
+
+                    <img
+                      src={`https://image.tmdb.org/t/p/w500${movie.poster_path} `}
+                      alt=""
+                      className="w-52 h-auto mx-auto lg:w-64 lg:mx-0"
+                    />
+                    <p className="text-white">{movie.overview}</p>
+                  </div>
+                )
+              : null}
           </div>
         ))}
       </div>
