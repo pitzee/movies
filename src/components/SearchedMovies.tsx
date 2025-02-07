d9554bc15780a71e602c1cf6d8a0160f8a65b40d
pitzee <pitzee421@gmail.com>
2025-02-07 07:03:45 +0000

    search functionality is added to searched movies

--- src/components/SearchedMovies.tsx
+++ src/components/SearchedMovies.tsx
@@ -1,10 +1,12 @@
 import Navbar from "./Navbar";
+import Search from "./Search";
 import SearchedMoviesGrid from "./SearchedMoviesGrid";
 
 const SearchedMovies = () => {
   return (
     <div>
       <Navbar />
+      <Search />
       <p className="text-2xl font-bold mb-5">Searched movies list</p>
       <SearchedMoviesGrid />
     </div>
